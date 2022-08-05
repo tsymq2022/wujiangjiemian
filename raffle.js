d585d6395b319a4004a35019e03ace8284d230f1
tsymq2022 <110645099+tsymq2022@users.noreply.github.com>
2022-08-05 10:50:24 +0800

    Update raffle.js

--- raffle.js
+++ raffle.js
@@ -4,5 +4,5 @@ window.raffle = {};
 window.raffle.version = true;
 window.raffle.date = [2022,8,5,20,30,0];
 window.raffle.list = ['sha','shan','tao','jiu','du','juedou','huogong','wuxie','nanman','wanjian','wugu','guohe','shunshou','tiesuo','jiedao','taoyuan','shandian','lebu','bingliang','caomu','bagua','renwang','qinggang','zhuge','zhangba','baiyin','tengjia','qilin','guding','qinglong'];
-window.raffle.prize = [['juedou','wuxie','du','shunshou','nanman'];
+window.raffle.prize = ['juedou','wuxie','du','shunshou','nanman'];
 window.raffle.last = ['wugu','sha','tao','qinglong','lebu'];
